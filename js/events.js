d0bb05c0a026335376f8b7cf80a2739711092f0d
JebusDev <antoniozuniga.dev@gmail.com>
2023-01-13 00:52:44 -0700

    color a input checkbox agregado

--- js/events.js
+++ js/events.js
@@ -11,14 +11,12 @@ const createButton = document.querySelector('#btnCreateTask');
 const ulFilter = document.querySelector('.filters');
 const category = document.querySelector('#filter-tag');
 let categorySelected;
-// let todosFilter;
 
 export const crearTodoHtml = ( todo ) => {
-
     const htmlTodo = `
     <li class="${ (todo.completado ? 'completed' : '' ) }" data-id="${ todo.id }">
             <span class="circle ${todo.category}"></span>
-            <input id="check-toggle" class="check-toggle" type="checkbox" ${ (todo.completado ? 'checked' : '' )}>
+            <input id="check-toggle" class="check-toggle ${todo.category}" type="checkbox" ${ (todo.completado ? 'checked' : '' )}>
             <label>${ todo.tarea }</label>
             <button class="destroy"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="3" y1="3" x2="21" y2="21"></line><path d="M4 7h3m4 0h9"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="14" x2="14" y2="17"></line><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l.077 -.923"></path><line x1="18.384" y1="14.373" x2="19" y2="7"></line><path d="M9 5v-1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path></svg></button>
     </li>
@@ -49,7 +47,9 @@ category.addEventListener('change', () =>{
 txtInput.addEventListener('keyup', (e) => {
 
     const task = txtInput.value.trim();
-    if ( e.keyCode === 13 && task.length > 0 && categorySelected != undefined) {
+    if (e.keyCode === 13 && categorySelected === undefined) {
+    alert('Selecciona una categoria');
+    } else if ( e.keyCode === 13 && task.length > 0 ) {
         const nuevoTodo = new Todo(task, categorySelected)
         todoList.newTodo( nuevoTodo );
         crearTodoHtml(nuevoTodo);
