e7cdbfed7b8adc3da0c9aee362949b548d1d5ce5
JebusDev <antoniozuniga.dev@gmail.com>
2023-01-25 23:27:40 -0700

    arreglando boton de eliminar todo's

--- js/events.js
+++ js/events.js
@@ -17,8 +17,8 @@ export const crearTodoHtml = ( todo ) => {
     <div class="${ (todo.completado ? 'completed' : '' ) } todo-container" data-id="${ todo.id }">
             <span class="circle ${todo.category}"></span>
             <input id="check-toggle" class="check-toggle ${todo.category}" type="checkbox" ${ (todo.completado ? 'checked' : '' )}>
-            <p>${ todo.tarea }</p>
-            <button class="destroy"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="3" y1="3" x2="21" y2="21"></line><path d="M4 7h3m4 0h9"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="14" x2="14" y2="17"></line><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l.077 -.923"></path><line x1="18.384" y1="14.373" x2="19" y2="7"></line><path d="M9 5v-1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path></svg></button>
+            <label>${ todo.tarea }</label>
+            <i class="fa-regular fa-trash-can destroy"></i>      
     </div>
     `;
 
@@ -64,10 +64,10 @@ divTodoList.addEventListener('click', (e) => {
     const todoElemento = e.target.parentElement;
     const todoId = todoElemento.getAttribute('data-id');
     if ( nombreElemento.includes('input') ) { 
-        todoList.toggleTodo( todoId );
+        todoList.toggleTodo( todoId ); 
         todoElemento.classList.toggle('completed');
         funcTodosDone();
-    } else if (nombreElemento.includes('button')) {
+    } else if (nombreElemento.includes('i')) {
         todoList.deleteTodo( todoId );
         divTodoList.removeChild( todoElemento );
         funcTodosDone();
@@ -83,7 +83,7 @@ btnDelDone.addEventListener('click', () => {
 createButton.addEventListener('click', () => {
     createContainer.style.display = "block";
     setTimeout(() => {
-        createContainer.classList.add("is-visible");
+        createContainer.classList.add("is-visible"); 
     }, 1);
 }) 
 
